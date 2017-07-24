6057a6f486981996fa0dee8ce82ab520a4851188
android-cloud <ylq1314@vip.qq.com>
2017-07-24 11:43:13 +0800

    thrid commit

---
+++ MyMapView/app/src/main/java/com/example/vmmet/mymapview/bean/Lasso.java
@@ -0,0 +1,44 @@
+package com.example.vmmet.mymapview.bean;
+
+import android.graphics.PointF;
+
+import java.util.List;
+
+/**
+ * Created by Vmmet on 2016/7/29.
+ */
+public class Lasso {
+    // polygon coordinates
+    private float[] mPolyX, mPolyY;
+    // number of size in polygon
+    private int mPolySize;
+    public Lasso(float[] px, float[] py, int ps) {
+        this.mPolyX = px;
+        this.mPolyY = py;
+        this.mPolySize = ps;
+    }
+    public Lasso(List<PointF> pointFs) {
+        this.mPolySize = pointFs.size();
+        this.mPolyX = new float[this.mPolySize];
+        this.mPolyY = new float[this.mPolySize];
+
+        for (int i = 0; i < this.mPolySize; i++) {
+            this.mPolyX[i] = pointFs.get(i).x;
+            this.mPolyY[i] = pointFs.get(i).y;
+        }
+       // Log.d("lasso", "lasso size:" + mPolySize);
+    }
+    public boolean contains(float x, float y) {
+        boolean result = false;
+        for (int i = 0, j = mPolySize - 1; i < mPolySize; j = i++) {
+            if ((mPolyY[i] < y && mPolyY[j] >= y)
+                    || (mPolyY[j] < y && mPolyY[i] >= y)) {
+                if (mPolyX[i] + (y - mPolyY[i]) / (mPolyY[j] - mPolyY[i])
+                        * (mPolyX[j] - mPolyX[i]) < x) {
+                    result = !result;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ MyMapView/app/src/main/java/com/example/vmmet/mymapview/bean/Province.java
@@ -0,0 +1,49 @@
+package com.example.vmmet.mymapview.bean;
+
+import android.graphics.Path;
+
+import java.util.List;
+
+/**
+ * Created by Vmmet on 2016/7/29.
+ */
+public class Province {
+    private String name;
+    private List<Path> listpath;
+    private int linecolor;
+
+    public int getLinecolor() {
+        return linecolor;
+    }
+
+    public void setLinecolor(int linecolor) {
+        this.linecolor = linecolor;
+    }
+
+    public List<Path> getListpath() {
+        return listpath;
+    }
+    public void setListpath(List<Path> listpath) {
+        this.listpath = listpath;
+    }
+    private List<Lasso> pathLasso;
+    public List<Lasso> getPathLasso() {
+        return pathLasso;
+    }
+    public void setPathLasso(List<Lasso> pathLasso) {
+        this.pathLasso = pathLasso;
+    }
+    private int color;
+    public int getColor() {
+        return color;
+    }
+    public void setColor(int color) {
+        this.color = color;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
